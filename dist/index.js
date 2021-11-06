47db5681b89af6beb863c1479a042070fefa79b8
LinaPeralta <69905738+LinaPeralta@users.noreply.github.com>
2021-11-06 15:21:08 -0500

    primer commit

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+
+
+
+
+const nombreEstudiante= document.getElementById("estudiante");
+const codigoEstudiante= document.getElementById("codigo");
+const cursoEstudiante= document.getElementById("curso");
+
+const matricularBoton = document.getElementById("matricularBtn");
+
+
+
+function test (){
+    alert("EA EA EA COMO TU MENTE MAQUINEA");
+}
+
+matricularBoton.addEventListener("click",test);
